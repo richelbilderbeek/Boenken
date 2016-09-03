258f95461f3827faa969e1d3884ba1db78bd2397
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-03 16:03:48 +0200

    Ignore Qt warning

--- qtboenkenspritenonmoving_test.h
+++ qtboenkenspritenonmoving_test.h
@@ -1,7 +1,11 @@
 #ifndef BOENKENSPRITENONMOVINGTEST_H
 #define BOENKENSPRITENONMOVINGTEST_H
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Weffc++"
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #include <QtTest/QtTest>
+#pragma GCC diagnostic pop
 
 namespace ribi {
 namespace bnkn {
