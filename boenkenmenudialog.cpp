e71a27e7150160cbe4bef1900b81d19dbd9302a4
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-04 11:38:18 +0100

    Compiles locally

--- boenkenmenudialog.cpp
+++ boenkenmenudialog.cpp
@@ -1,24 +1,3 @@
-//---------------------------------------------------------------------------
-/*
-Boenken. A multiplayer soccer/billiards game.
-Copyright (C) 2007-2016 Richel Bilderbeek
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-//---------------------------------------------------------------------------
-//From http://www.richelbilderbeek.nl/GameBoenken.htm
-//---------------------------------------------------------------------------
 #include "boenkenmenudialog.h"
 
 #include <cassert>
@@ -28,8 +7,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "fileio.h"
 #include "geometry.h"
 #include "plane.h"
-#include "testtimer.h"
-#include "trace.h"
 
 ribi::bnkn::MenuDialog::MenuDialog()
 {
@@ -41,7 +18,7 @@ int ribi::bnkn::MenuDialog::ExecuteSpecific(const std::vector<std::string>& argv
   if (argc == 1)
   {
     std::cout << GetHelp() << '\n';
-    return 1;
+    return 0;
   }
   std::cout
     << this->GetAbout().GetFileTitle() << " cannot be run in console mode\n"
@@ -65,8 +42,6 @@ ribi::About ribi::bnkn::MenuDialog::GetAbout() const noexcept
   a.AddLibrary("Container version: " + Container().GetVersion());
   a.AddLibrary("FileIo version: " + fileio::FileIo().GetVersion());
   a.AddLibrary("Geometry version: " + Geometry().GetVersion());
-  a.AddLibrary("Plane version: " + Plane::GetVersion());
-  a.AddLibrary("TestTimer version: " + TestTimer::GetVersion());
   return a;
 }
 
